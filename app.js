63d4838d53f7fb13c5d6f4ef1b3d610b2df7bad6
CN24 <cn24@Dans-MacBook-Pro-2.local>
2019-09-17 17:36:51 +0100

    testing

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 const express = require("express")
 
 const app = express();
+
+app.get("/", (req, res) => {
+    res.send('<h1>Hi my name is Tom</h1>');
+});
+
+app.get("/me", (req, res) => {
+    res.send({name: "tom", age: 27})
+})
+
+app.get("/cake", (req, res) => {
+    console.log('MMMMmmmmhhhhh cake')
+})
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000")
+})
